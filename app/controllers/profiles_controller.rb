7baad9d0385a1eedcb13ad5fa61ce920c9286b43
archae0pteryx <aarchaeopteryxx@gmail.com>
2021-11-19 11:47:30 -0800

    add create profile logic and page

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -1,2 +1,10 @@
 class ProfilesController < ApplicationController
+  def create
+    @profile = Profile.new(profile_params)
+    if @profile.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
 end
--- app/javascript/components/App.js
+++ app/javascript/components/App.js
@@ -1,32 +1,27 @@
-import React from "react"
-import Header from './components/Header'
-import Footer from './components/Footer'
-import Home from './pages/Home'
-import Songs from './pages/Songs'
-import Users from './pages/Users'
-import Profile from './pages/Profile'
-import AboutUs from './pages/AboutUs'
-
-import {
-  BrowserRouter,
-  Switch,
-  Route
-} from 'react-router-dom'
+import React from "react";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import Home from "./pages/Home";
+import Songs from "./pages/Songs";
+import Users from "./pages/Users";
+import Profile from "./pages/Profile";
+import AboutUs from "./pages/AboutUs";
+import CreateProfile from "./pages/CreateProfile";
+import { BrowserRouter, Switch, Route } from "react-router-dom";
 
 class App extends React.Component {
-  render () {
-    console.log(this.props)
-    return (
+  render() {
+    return this.props.logged_in && this.props.profile.length === 0 ? (
+      <CreateProfile {...this.props} />
+    ) : (
       <BrowserRouter>
-      <Header {...this.props}/>
-      
+        <Header {...this.props} />
         <Switch>
           <Route exact path="/" component={Home} />
           <Route path="/songindex" component={Songs} />
           <Route path="/users" component={Users} />
           <Route path="/profile" component={Profile} />
           <Route path="/aboutus" component={AboutUs} />
-
         </Switch>
         <Footer />
       </BrowserRouter>
@@ -34,4 +29,4 @@ class App extends React.Component {
   }
 }
 
-export default App
+export default App;
