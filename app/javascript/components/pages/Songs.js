d56999c5b56333ccc6ddcdf0faf0e04c27217ad1
brandonho2000 <bradho099@gmail.com>
2021-11-22 18:41:14 -0600

    Table for index

--- app/javascript/components/pages/Songs.js
+++ app/javascript/components/pages/Songs.js
@@ -1,23 +1,39 @@
 import React, { Component } from "react";
-
+import { thead, tr, th, tbody, Table, hover } from "reactstrap";
 class Songs extends Component {
   render() {
-    const { songs } = this.props
-    console.log(songs)
+    const { songs } = this.props;
+    console.log(songs);
     return (
       <>
         <h1>This is songs</h1>
-        <div>
-          {songs && songs.map(song =>{
-            return(
-              <ul>
-                <li key={song.id}> {song.song_name} , {song.song_artist} , {song.genre}</li>
-              </ul> 
 
-            )
-          })}
-        
-        </div>
+        <Table hover>
+          <thead>
+            <tr>
+              <th>Artist</th>
+              <th>Song</th>
+              <th>Genre</th>
+            </tr>
+          </thead>
+          {songs &&
+            songs.map((song) => {
+              return (
+                <tbody>
+                  <tr>
+                    {/* <th scope="row"></th> */}
+                    <td>{song.song_artist}</td>
+                    <td>{song.song_name}</td>
+                    <td>{song.genre}</td>
+                  </tr>
+                </tbody>
+              );
+            })}
+        </Table>
+
+        {/* <ul>
+                   <li key={song.id}> {song.song_name} , {song.song_artist} , {song.genre}</li>
+                 </ul> */}
       </>
     );
   }
