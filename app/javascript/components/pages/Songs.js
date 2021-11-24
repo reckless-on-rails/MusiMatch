5fffac75f0d8f541f175b9e0c2870c0aadec303b
hackcoder77 <hackcoder77@gmail.com>
2021-11-23 16:18:29 -0800

    song index styling

--- app/javascript/components/pages/Songs.js
+++ app/javascript/components/pages/Songs.js
@@ -1,26 +1,34 @@
 import React, { Component } from "react";
 import { thead, tr, th, tbody, Table, hover } from "reactstrap";
+
 class Songs extends Component {
   render() {
     const { songs } = this.props;
     return (
       <>
-        <h1>This is songs</h1>
+      <th>
+        <li>
+        <h1>Library</h1>
+        </li>
 
+        
         <Table hover>
           <thead>
             <tr>
+              <th>Cover</th>
               <th>Artist</th>
               <th>Song</th>
               <th>Genre</th>
             </tr>
           </thead>
+          
           {songs &&
             songs.map((song, i) => {
               return (
                 <tbody key={i}>
                   <tr>
                     {/* <th scope="row"></th> */}
+                    <td><img src={song.cover_url} width="75px" height="75px"></img></td>
                     <td>{song.song_artist}</td>
                     <td>{song.song_name}</td>
                     <td>{song.genre}</td>
@@ -29,6 +37,7 @@ class Songs extends Component {
               );
             })}
         </Table>
+        </th>
 
         {/* <ul>
                    <li key={song.id}> {song.song_name} , {song.song_artist} , {song.genre}</li>
