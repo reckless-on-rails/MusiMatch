5bc2527585b1b72393350e96d624bf8cd72a865c
hackcoder77 <hackcoder77@gmail.com>
2021-11-24 10:30:25 -0800

    cover url, fonts and bg again

--- app/javascript/components/pages/Songs.js
+++ app/javascript/components/pages/Songs.js
@@ -1,49 +1,49 @@
 import React, { Component } from "react";
-import { thead, tr, th, tbody, Table} from "reactstrap";
+import { thead, tr, th, tbody, Table } from "reactstrap";
 
 class Songs extends Component {
   render() {
     const { songs } = this.props;
     return (
       <>
-      <th>
-        < div className ="barcode">
-        <h1>Library</h1>
-        </div>
+        <th>
+          <div className="barcode">
+            <h1>Library</h1>
+          </div>
 
-        
-        <Table hover>
-          <thead>
-            <tr>
-              <th>COVER</th>
-              <th>ARTIST</th>
-              <th>SONG</th>
-              <th>GENRE</th>
-              
-            </tr>
-          </thead>
-          <br></br>
-          
-          {songs &&
-            songs.map((song, i) => {
-              return (
-                <tbody key={i}>
-                  <tr>
-                    {/* <th scope="row"></th> */}
-                    <td><img src={song.cover_url} width="75px" height="75px"></img></td>
-                    <td>{song.song_artist}</td>
-                    <td>{song.song_name}</td>
-                    <td>{song.genre}</td>
-                  </tr>
-                </tbody>
-              );
-            })}
-        </Table>
+          <Table hover>
+            <thead>
+              <tr>
+                <th>COVER</th>
+                <th>ARTIST</th>
+                <th>SONG</th>
+                <th>GENRE</th>
+              </tr>
+            </thead>
+            <br></br>
+
+            {songs &&
+              songs.map((song, i) => {
+                return (
+                  <tbody key={i}>
+                    <tr>
+                      {/* <th scope="row"></th> */}
+                      <td>
+                        <img
+                          src={song.cover_url}
+                          width="75px"
+                          height="75px"
+                        ></img>
+                      </td>
+                      <td>{song.song_artist}</td>
+                      <td>{song.song_name}</td>
+                      <td>{song.genre}</td>
+                    </tr>
+                  </tbody>
+                );
+              })}
+          </Table>
         </th>
-
-        {/* <ul>
-                   <li key={song.id}> {song.song_name} , {song.song_artist} , {song.genre}</li>
-                 </ul> */}
       </>
     );
   }
