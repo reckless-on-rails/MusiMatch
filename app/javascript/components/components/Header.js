9dea36fb849e9eabdf70ff0d5338b8df44c66647
ciannadogan <dogan.cianna@yahoo.com>
2021-11-24 16:07:19 -0800

    testing for header, footer, and home

--- app/javascript/components/components/Header.js
+++ app/javascript/components/components/Header.js
@@ -1,6 +1,5 @@
 import React, { Component } from "react";
 import { NavLink } from "react-router-dom";
-import tape from "../assets/tape8.gif";
 
 class Header extends Component {
   render() {
@@ -14,7 +13,7 @@ class Header extends Component {
     return (
       <header>
         <NavLink to="/">
-          <img src={tape} alt="logo for musimatch" className="logo"></img>
+          <img src="../assets/tape8.gif" alt="logo for musimatch" className="logo"></img>
         </NavLink>
         <NavLink to="/songindex">songindex</NavLink>
         <NavLink to="/users">Users</NavLink>
